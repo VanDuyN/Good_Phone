7a2b14b1269c9324a2f76fbff60b43b306e5d5ca
VanDuyN <vanduyfe@gmail.com>
2023-10-10 19:00:47 +0700

    update login/dangky check user phan quyen auto login done

--- app/src/main/java/com/example/goodphone/LogIn.java
+++ app/src/main/java/com/example/goodphone/LogIn.java
@@ -7,6 +7,7 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,9 +17,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.StartupTime;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,12 +35,16 @@ public class LogIn extends AppCompatActivity {
     private EditText edtEmail,edtPassword;
     private TextView btnRegisterNow;
     private ProgressDialog progressDialog;
+    String uid;
+    FirebaseUser user;
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
-        unit();
+        init();
         button();
 
     }
@@ -59,7 +70,7 @@ public class LogIn extends AppCompatActivity {
         String email = edtEmail.getText().toString().trim();
         String password = edtPassword.getText().toString().trim();
         //Goi firebas Auth
-        FirebaseAuth auth = FirebaseAuth.getInstance();
+
 
         //validate email
         String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
@@ -68,6 +79,8 @@ public class LogIn extends AppCompatActivity {
 
             //kiểm tra tài khoản mật khẩu
             if (password.trim().length() >= 8){
+                progressDialog.setMessage("Đang đăng nhập vui lòng chờ");
+                progressDialog.setCancelable(false);
                 progressDialog.show();
 
                 // kiểm tra email password trên hệ thống
@@ -76,11 +89,9 @@ public class LogIn extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    progressDialog.dismiss();
-                                    Intent i = new Intent(LogIn.this, MainActivity.class);
-                                    Toast.makeText(LogIn.this, "Đăng nhập thành công",Toast.LENGTH_SHORT).show();
-                                    startActivity(i);
-                                    finishAffinity();
+                                    user = FirebaseAuth.getInstance().getCurrentUser();
+                                    uid = user.getUid();
+                                    checkUser();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     progressDialog.dismiss();
@@ -91,10 +102,12 @@ public class LogIn extends AppCompatActivity {
             }
             else {
                 Toast.makeText(LogIn.this, "Vui lòng Kiểm tra lại mật khẩu mật khẩu phải từ 8 kí tự trở lên",Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
                 return;
             }
         }else {
             Toast.makeText(LogIn.this, "vui lòng iểm tra lại email",Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
             return;
         }
     }
@@ -103,11 +116,51 @@ public class LogIn extends AppCompatActivity {
         Matcher matcher = pattern.matcher(input);
         return matcher.matches();
     }
-    public void unit(){
+    public void init(){
         btnLogin = findViewById(R.id.btnLogin);
         btnRegisterNow = findViewById(R.id.RegisterNow);
         edtEmail = findViewById(R.id.edtEmailLogin);
         edtPassword = findViewById(R.id.edtPassLogin);
         progressDialog = new ProgressDialog(this);
+        auth = FirebaseAuth.getInstance();
+
+        firestore = FirebaseFirestore.getInstance();
+    }
+    public void checkUser(){
+        firestore.collection("User")
+                .document(uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                // Lấy giá trị của một trường con trong document
+                                String role = document.getString("role");
+                                Toast.makeText(LogIn.this, role,Toast.LENGTH_SHORT).show();
+                                if (role.equals("admin") ){
+                                    progressDialog.dismiss();
+                                    Intent i =  new Intent(LogIn.this,Admin_Home.class);
+                                    startActivity(i);
+                                    finishAffinity();
+                                }else {
+                                    progressDialog.dismiss();
+                                    Intent i =  new Intent(LogIn.this,MainActivity.class);
+                                    startActivity(i);
+                                    finishAffinity();
+                                }
+                            } else {
+                                progressDialog.dismiss();
+                                Toast.makeText(LogIn.this, "Tài khoản lỗi",Toast.LENGTH_SHORT).show();
+                                auth.signOut();
+                            }
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(LogIn.this, "Tài khoản lỗi",Toast.LENGTH_SHORT).show();
+                            auth.signOut();
+                        }
+                    }
+                });
     }
 }
