7ef079486a8e31024d70549b21208d3288c3c7cd
VanDuyN <vanduyfe@gmail.com>
2023-10-13 23:34:47 +0700

    update code phu

--- app/src/main/java/com/example/goodphone/QLSanPhamActivity.java
+++ app/src/main/java/com/example/goodphone/QLSanPhamActivity.java
@@ -1,20 +1,42 @@
 package com.example.goodphone;
 
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.goodphone.adapter.Product_adapter;
 import com.example.goodphone.adapter.RecyclerViewItemTouchHelper;
 import com.example.goodphone.adapter.SanPhamAdapter;
+import com.example.goodphone.fragment.Add_Product;
+import com.example.goodphone.model.List_Product;
 import com.example.goodphone.model.SanPham;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,44 +44,107 @@ import java.util.List;
 public class QLSanPhamActivity extends AppCompatActivity implements ItemTouchHelperListener {
     private RecyclerView rcvSanPham;
     private SanPhamAdapter adapter;
-    private List<SanPham> mListSanPham;
+    private List<List_Product> mListSanPham;
     private RelativeLayout rootView;
+    StorageReference storageRef, imageRef;
+    FirebaseUser user;
+    FirebaseAuth auth;
+    FirebaseFirestore dbProduct;
+    ImageButton btnExit;
+    Button btnAddProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qlsanpham);
+        init();
+        setClick();
+        getListSanPham();
+
+    }
+    public void setClick(){
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendDataToFragment();
+            }
+        });
+
+    }
+    public void sendDataToFragment(){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.add_Product_Fragment,new Add_Product());
+        fragmentTransaction.commit();
+    }
+    private void getListSanPham(){
+        List<List_Product> arrProduct= new ArrayList<>();
+        dbProduct.collection("Product").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String id = document.getId();
+                                Log.e("acv",id);
+                                String name = document.getString("Name".trim());
+                                FirebaseStorage storage = FirebaseStorage.getInstance("gs://goodphone-687e7.appspot.com/");
+                                storageRef = storage.getReference().child("Product");
+                                imageRef = storageRef.child(name +".jpg");
+                                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String fileUrl = uri.toString();
+                                        arrProduct.add(new List_Product(id,fileUrl,name));
+                                        adapter = new SanPhamAdapter(QLSanPhamActivity.this,arrProduct);
+                                        rcvSanPham.setAdapter(adapter);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception exception) {
+
+                                    }
+                                });
+                            }
+                        } else {
+                            Toast.makeText(QLSanPhamActivity.this, "lỗi",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    public void init(){
+        auth = FirebaseAuth.getInstance();
+        user  = auth.getCurrentUser();
+        dbProduct = FirebaseFirestore.getInstance();
+        btnAddProduct = findViewById(R.id.btn_Add_Product);
         rootView= findViewById(R.id.root_view);
-
         rcvSanPham = findViewById(R.id.rcv_sanpham);
+
+        btnExit =  findViewById(R.id.btn_Exit_QLSP);
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
         rcvSanPham.setLayoutManager(linearLayoutManager);
-
-        mListSanPham = getListSanPham();
-        adapter = new SanPhamAdapter(mListSanPham);
-        rcvSanPham.setAdapter(adapter);
-
         RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
         rcvSanPham.addItemDecoration(itemDecoration);
         ItemTouchHelper.SimpleCallback simpleCallback = new RecyclerViewItemTouchHelper(0,ItemTouchHelper.LEFT,this);
 
         new ItemTouchHelper(simpleCallback).attachToRecyclerView(rcvSanPham);
     }
-    private List<SanPham> getListSanPham(){
-        List<SanPham> list = new ArrayList<>();
-        for(int i = 0; i<20; i++){
-            list.add(new SanPham("Phone 1"+(i +1),R.drawable.img));
-        }
-        return list;
-    }
 
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder) {
         if(viewHolder instanceof SanPhamAdapter.SanPhamViewHolder){
-            String nameSanPhamDelete = mListSanPham.get(viewHolder.getAdapterPosition()).getName();
-            Integer imageSanPhamDelete = mListSanPham.get(viewHolder.getAdapterPosition()).getImageId();
+            String nameSanPhamDelete = mListSanPham.get(viewHolder.getAdapterPosition()).getNameProduct();
 
-            SanPham sanPhamDelete = mListSanPham.get(viewHolder.getAdapterPosition());
+            Integer imageSanPhamDelete = mListSanPham.get(viewHolder.getAdapterPosition()).getImage_Main();
+
+            List_Product sanPhamDelete = mListSanPham.get(viewHolder.getAdapterPosition());
             int indexDelete = viewHolder.getAdapterPosition();
 
             adapter.removeItem(indexDelete);
--- app/src/main/java/com/example/goodphone/adapter/SanPhamAdapter.java
+++ app/src/main/java/com/example/goodphone/adapter/SanPhamAdapter.java
@@ -1,5 +1,7 @@
 package com.example.goodphone.adapter;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +12,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.goodphone.R;
-import com.example.goodphone.model.SanPham;
+import com.example.goodphone.model.List_Product;
 
 import java.util.List;
 
 public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.SanPhamViewHolder>{
-    private List<SanPham> mListSanPham;
-    public SanPhamAdapter(List<SanPham> mListSanPham){
+    private List<List_Product> mListSanPham;
+    Context context;
+    public SanPhamAdapter(List<List_Product> mListSanPham){
+        this.mListSanPham = mListSanPham;
+    }
+    public SanPhamAdapter(Context context,List<List_Product> mListSanPham){
+        this.context = context;
         this.mListSanPham = mListSanPham;
     }
     @NonNull
@@ -29,12 +37,15 @@ public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.SanPhamV
 
     @Override
     public void onBindViewHolder(@NonNull SanPhamViewHolder holder, int position) {
-        SanPham sanPham = mListSanPham.get(position);
+        List_Product sanPham = mListSanPham.get(position);
         if (sanPham == null){
             return;
         }
-        holder.tvName.setText(sanPham.getName());
-        holder.imageView.setImageResource(sanPham.getImageId());
+        holder.tvName.setText(sanPham.nameProduct);
+        holder.imageView.setImageResource(sanPham.image_Main);
+        String image = mListSanPham.get(position).url_img_product;
+        Glide.with(context).load(image).centerCrop().into(holder.imageView);
+        Log.e("test",image);
     }
 
     @Override
@@ -55,13 +66,14 @@ public class SanPhamAdapter extends RecyclerView.Adapter<SanPhamAdapter.SanPhamV
             tvName = itemView.findViewById(R.id.tv_name);
             imageView = itemView.findViewById(R.id.item_image_view);
             layoutForeGround = itemView.findViewById(R.id.layout_foreground);
+
         }
     }
     public void removeItem (int index){
         mListSanPham.remove(index);
         notifyItemRemoved(index);
     }
-    public void undoItem(SanPham sanPham, int index){
+    public void undoItem(List_Product sanPham, int index){
         mListSanPham.add(index,sanPham);
         notifyItemInserted(index);
     }
