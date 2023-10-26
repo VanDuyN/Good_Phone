508b24ca4bcc523fc17a47197cffa2391aa9b6b4
thanh <thanhtung03122003@gmail.com>
2023-10-26 20:17:56 +0700

    EmTunwdg

---
+++ app/src/main/java/com/example/goodphone/model/User.java
@@ -0,0 +1,27 @@
+package com.example.goodphone.model;
+
+public class User {
+    private int imageId;
+    private String name;
+
+    public User(int imageId, String name) {
+        this.imageId = imageId;
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
